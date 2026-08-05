24a24016d3102d82674565da274926b67103317b
Ingo Karkat <swdev@ingo-karkat.de>
2026-08-05 10:10:29 +0200

    ENH: Add dl "debug list targets" alias
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -10,13 +10,21 @@ alias d='withDebug'
 IFS=: optionmunge HISTIGNORE 'd:d *'
 alias da='withDebuga'
 IFS=: optionmunge HISTIGNORE 'da:da *'
+
+alias dl='withDebug --list-targets'
+IFS=: optionmunge HISTIGNORE 'dl:dl *'
+alias dla='withDebuga --list-targets'
+IFS=: optionmunge HISTIGNORE 'dla:dla *'
+
 alias dt='withDebug --extension \&'
 IFS=: optionmunge HISTIGNORE 'dt:dt *'
 alias dta='withDebuga --extension \&'
 IFS=: optionmunge HISTIGNORE 'dta:dta *'
+
 alias ds='withDebug --same'
 IFS=: optionmunge HISTIGNORE 'ds:ds *'
 alias dsa='withDebuga --same'
 IFS=: optionmunge HISTIGNORE 'dsa:dsa *'
+
 alias dr='withDebug --recall'
 IFS=: optionmunge HISTIGNORE 'dr:dr *'
