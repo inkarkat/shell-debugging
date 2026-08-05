bc3db66115f8ce7d84e1d9e3cee85bf5969b8095
Ingo Karkat <swdev@ingo-karkat.de>
2026-08-05 09:04:34 +0200

    aliases: FIX: Add recently added command-line arguments to addAliasSupport
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,10 +1,10 @@
 #!/bin/sh source-this-script
 
 addAliasSupport withDebug \
-    '12IPTaistv' \
-    'stdout|stderr|append|no-pager|inbox|to-separate-files|to-tty|no-timestamps|interactive|verbose' \
+    '12IPTailstv' \
+    'list-targets|same|stdout|stderr|append|no-pager|inbox|to-separate-files|to-stderr|to-tty|no-timestamps|interactive|verbose' \
     'Mmx' \
-    'extension|for|module|only-module'
+    'extension|for|module|only-for|only-module'
 
 alias d='withDebug'
 IFS=: optionmunge HISTIGNORE 'd:d *'
