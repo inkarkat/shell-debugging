b1cb3ae4632851f5a3a5429c94c03139f844ecb2
Ingo Karkat <swdev@ingo-karkat.de>
2026-08-22 10:18:04 +0200

    aliases: ENH: Complete command
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -10,6 +10,7 @@ alias d='withDebug'
 optionmunge -s : HISTIGNORE 'd:d *'
 alias da='withDebuga'
 optionmunge -s : HISTIGNORE 'da:da *'
+completeAsCommand d da
 
 if [ ! "${KSH_VERSION:-}" ]; then
     # Korn shell doesn't support ? in alias names.
@@ -17,17 +18,20 @@ if [ ! "${KSH_VERSION:-}" ]; then
     optionmunge -s : HISTIGNORE 'd?:d? *'
     alias 'd?a'='withDebuga --list-targets'
     optionmunge -s : HISTIGNORE 'd?a:d?a *'
+    completeAsCommand 'd?' 'd?a'
 fi
 
 alias dt='withDebug --extension \&'
 optionmunge -s : HISTIGNORE 'dt:dt *'
 alias dta='withDebuga --extension \&'
 optionmunge -s : HISTIGNORE 'dta:dta *'
+completeAsCommand dt dta
 
 alias ds='withDebug --same'
 optionmunge -s : HISTIGNORE 'ds:ds *'
 alias dsa='withDebuga --same'
 optionmunge -s : HISTIGNORE 'dsa:dsa *'
+completeAsCommand ds dsa
 
 alias dr='withDebug --recall'
 optionmunge -s : HISTIGNORE 'dr:dr *'
