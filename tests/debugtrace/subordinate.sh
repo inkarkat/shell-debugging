ea4b4977ccadc9c56794d619eb47efe454b39a9e
Ingo Karkat <swdev@ingo-karkat.de>
2026-08-23 13:22:07 +0200

    withDebug: ENH: Introduce DEBUG_LOGDIR, default to /run/user/$UID/debug, and only fall back to $PWD if explicitly empty
    So any debug logs are now by default collected in a dedicated directory, instead of cluttering the PWDs of all instrumented executions.
    
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release

--- tests/debugtrace/subordinate.sh
+++ tests/debugtrace/subordinate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ${DEBUG:+debuglog This is the script invoked by the main script.}
-${DEBUG:+eval $(debugtrace --description 'subordinate script' --to subordinate.trc.log -o $- -- "$@")}
+${DEBUG:+eval $(debugtrace --description 'subordinate script' --to "${DEBUG_LOGDIR}${DEBUG_LOGDIR:+/}subordinate.trc.log" -o $- -- "$@")}
 
 echo "This is $*."
